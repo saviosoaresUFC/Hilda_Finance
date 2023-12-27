d7f0b5ccff09275d48c593f69292029c663b57e3
SavioUFC <saviocarvalho@alu.ufc.br>
2023-12-27 11:24:41 -0300

    edition file

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import {
     StyleSheet, Text, View,
     Image, TouchableOpacity, ToastAndroid,
@@ -6,104 +6,179 @@ import {
 } from 'react-native'
 import { Entypo, Ionicons } from '@expo/vector-icons';
 import { COLORS, ICONS } from '../theme/theme';
-import { useStore } from '../store/store';
 
-const CardItem = () => {
-    const [mostrarIcones, setMostrarIcones] = useState(false);
-    const ListRef: any = useRef<FlatList>();
+interface CardItemProps {
+    buttonToggleBag: any;
+    buttonPressHandler: any;
+    SkewerSimpleList: any;
+    SkewerCompleteList: any;
+    HamburguerList: any;
+    FoodList: any;
+    DrinksList: any;
+}
 
-    const toggleIcones = () => {
-        setMostrarIcones(!mostrarIcones);
+const CardItem: React.FC<CardItemProps> = ({
+    buttonToggleBag,
+    buttonPressHandler,
+    SkewerSimpleList,
+    SkewerCompleteList,
+    HamburguerList,
+    FoodList,
+    DrinksList,
+}) => {
+    const [mostrarIconesSKWS, setMostrarIconesSKWS] = useState(false);
+    const [mostrarIconesSKWC, setMostrarIconesSKWC] = useState(false);
+    const [mostrarIconesHamburguer, setMostrarIconesHamburguer] = useState(false);
+    const [mostrarIconesFood, setMostrarIconesFood] = useState(false);
+    const [mostrarIconesDrink, setMostrarIconesDrink] = useState(false);
+
+    const toggleIconesSKWS = () => {
+        setMostrarIconesSKWS(!mostrarIconesSKWS);
+    };
+    const toggleIconesSKWC = () => {
+        setMostrarIconesSKWC(!mostrarIconesSKWC);
+    };
+    const toggleIconesHamburguer = () => {
+        setMostrarIconesHamburguer(!mostrarIconesHamburguer);
+    };
+    const toggleIconesFood = () => {
+        setMostrarIconesFood(!mostrarIconesFood);
+    };
+    const toggleIconesDrink = () => {
+        setMostrarIconesDrink(!mostrarIconesDrink);
     };
 
-    const SkewerSimpleList = useStore((state: any) => state.SkewerSimpleList);
-    const SkewerCompleteList = useStore((state: any) => state.SkewerCompleteList);
-    const HamburguerList = useStore((state: any) => state.HamburguerList);
-    const FoodList = useStore((state: any) => state.FoodList);
-    const DrinksList = useStore((state: any) => state.DrinkList);
-    const addToCart = useStore((state: any) => state.addToCart);
-    const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
 
-    const FoodCardAddToCart = ({
-        id,
-        index,
-        name,
-        prices,
-        type,
-    }: any) => {
-        addToCart({
-            id,
-            index,
-            name,
-            prices,
-            type,
-        });
-        calculateCartPrice();
-        ToastAndroid.showWithGravity(`${name} adicionado ao carrinho`,
-            ToastAndroid.SHORT,
-            ToastAndroid.CENTER
-        );
-    }
 
     return (
         <View style={styles.container}>
             <View style={styles.contentType}>
-                <TouchableOpacity onPress={toggleIcones} style={styles.buttonPress}>
+                <TouchableOpacity onPress={toggleIconesSKWS} style={styles.buttonPress}>
                     <Image source={ICONS.skewer_simple} style={styles.img} />
-                    <Text>Espeto Simples</Text>
+                    <View style={styles.viewTextTitle}>
+                        <Text style={styles.textTitle}>Espeto Simples</Text>
+                    </View>
                     <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
                 </TouchableOpacity>
             </View>
-            {SkewerSimpleList.map((item) => (
-                <View key={item.id} style={styles.modalContent}>
-                    <TouchableOpacity
-                        style={styles.buttonPressAdd}
-                        onPress={() => FoodCardAddToCart(item)}
-                    >
-                        <Ionicons name="add-circle-outline" size={24} color="black" />
-                        <Text style={styles.textNameSkewer}>{item.name}</Text>
-                    </TouchableOpacity>
-                </View>
-            ))}
-            <View style={styles.contentType}>
-                <TouchableOpacity onPress={toggleIcones} style={styles.buttonPress}>
-                    <Image source={ICONS.skewer} style={styles.img} />
-                    <Text>Espeto Completo</Text>
-                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.contentType}>
-                <TouchableOpacity onPress={toggleIcones} style={styles.buttonPress}>
-                    <Image source={ICONS.hamburguer} style={styles.img} />
-                    <Text>Hamburguer       </Text>
-                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.contentType}>
-                <TouchableOpacity onPress={toggleIcones} style={styles.buttonPress}>
-                    <Image source={ICONS.food} style={styles.img} />
-                    <Text>Arrumadinho       </Text>
-                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.contentType}>
-                <TouchableOpacity onPress={toggleIcones} style={styles.buttonPress}>
-                    <Image source={ICONS.drink} style={styles.img} />
-                    <Text>Bebidas               </Text>
-                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
-                </TouchableOpacity>
-            </View>
-            {mostrarIcones ? (
-                <View style={styles.cart}>
-                    <TouchableOpacity style={styles.buttonPressCart}>
-                        <Image source={ICONS.bag} style={styles.imgBag} />
-                    </TouchableOpacity>
-                </View>
-            ) : (
-                <></>
-            )
-            }
+            {mostrarIconesSKWS ? (
+                SkewerSimpleList.map((item) => (
+                    <View key={item.id} style={styles.modalContent}>
+                        <TouchableOpacity
+                            style={styles.buttonPressAdd}
+                            onPress={() => {
+                                buttonPressHandler(item);
+                                // console.log(item);
+                                buttonToggleBag();
+                            }}
+                        >
+                            <Ionicons name="add-circle-outline" size={24} color={COLORS.grayescuro} />
+                            <Text style={styles.textNameSubTitle}>{item.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            ) : <></>}
 
+            <View style={styles.contentType}>
+                <TouchableOpacity onPress={toggleIconesSKWC} style={styles.buttonPress}>
+                    <Image source={ICONS.skewer} style={styles.img} />
+                    <View style={styles.viewTextTitle}>
+                        <Text style={styles.textTitle}>Espeto Completo</Text>
+                    </View>
+                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            {mostrarIconesSKWC ? (
+                SkewerCompleteList.map((item) => (
+                    <View key={item.id} style={styles.modalContent}>
+                        <TouchableOpacity
+                            style={styles.buttonPressAdd}
+                            onPress={() => {
+                                buttonPressHandler(item)
+                                buttonToggleBag();
+                            }}
+                        >
+                            <Ionicons name="add-circle-outline" size={24} color={COLORS.grayescuro} />
+                            <Text style={styles.textNameSubTitle}>{item.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            ) : <></>}
+            <View style={styles.contentType}>
+                <TouchableOpacity onPress={toggleIconesHamburguer} style={styles.buttonPress}>
+                    <Image source={ICONS.hamburguer} style={styles.img} />
+                    <View style={styles.viewTextTitle}>
+                        <Text style={styles.textTitle}>Hamburguer</Text>
+                    </View>
+                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            {mostrarIconesHamburguer ? (
+                HamburguerList.map((item) => (
+                    <View key={item.id} style={styles.modalContent}>
+                        <TouchableOpacity
+                            style={styles.buttonPressAdd}
+                            onPress={() => {
+                                buttonPressHandler(item)
+                                buttonToggleBag();
+                            }}
+                        >
+                            <Ionicons name="add-circle-outline" size={24} color={COLORS.grayescuro} />
+                            <Text style={styles.textNameSubTitle}>{item.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            ) : <></>}
+            <View style={styles.contentType}>
+                <TouchableOpacity onPress={toggleIconesFood} style={styles.buttonPress}>
+                    <Image source={ICONS.food} style={styles.img} />
+                    <View style={styles.viewTextTitle}>
+                        <Text style={styles.textTitle}>Arrumadinho</Text>
+                    </View>
+                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            {mostrarIconesFood ? (
+                FoodList.map((item) => (
+                    <View key={item.id} style={styles.modalContent}>
+                        <TouchableOpacity
+                            style={styles.buttonPressAdd}
+                            onPress={() => {
+                                buttonPressHandler(item)
+                                buttonToggleBag();
+                            }}
+                        >
+                            <Ionicons name="add-circle-outline" size={24} color={COLORS.grayescuro} />
+                            <Text style={styles.textNameSubTitle}>{item.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            ) : <></>}
+            <View style={styles.contentType}>
+                <TouchableOpacity onPress={toggleIconesDrink} style={styles.buttonPress}>
+                    <Image source={ICONS.drink} style={styles.img} />
+                    <View style={styles.viewTextTitle}>
+                        <Text style={styles.textTitle}>Bebidas</Text>
+                    </View>
+                    <Entypo name="chevron-thin-down" size={24} color="black" style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+            {mostrarIconesDrink ? (
+                DrinksList.map((item) => (
+                    <View key={item.id} style={styles.modalContent}>
+                        <TouchableOpacity
+                            style={styles.buttonPressAdd}
+                            onPress={() => {
+                                buttonPressHandler(item)
+                                buttonToggleBag();
+                            }}
+                        >
+                            <Ionicons name="add-circle-outline" size={24} color={COLORS.grayescuro} />
+                            <Text style={styles.textNameSubTitle}>{item.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            ) : <></>}
         </View>
     )
 }
@@ -113,13 +188,13 @@ const styles = StyleSheet.create({
         flex: 1,
         position: 'relative',
         alignItems: 'center',
-        paddingBottom: '30%',
-        backgroundColor: 'blue',
+        // paddingBottom: '%',
     },
     contentType: {
         justifyContent: 'center',
         alignItems: 'center',
         height: '14%',
+        width: '109.5%',
     },
     buttonPress: {
         borderRadius: 10,
@@ -130,6 +205,7 @@ const styles = StyleSheet.create({
         height: '90%',
         width: '75%',
     },
+    
     img: {
         width: 40,
         height: 40,
@@ -138,51 +214,38 @@ const styles = StyleSheet.create({
     icon: {
         marginRight: '6%',
     },
-    cart: {
-        position: 'absolute',
-        backgroundColor: COLORS.orange,
-        borderRadius: 100,
-        height: '8%',
-        width: '12%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: '6%',
-        marginBottom: '6%',
-        left: '75%',
-        bottom: '118%',
-    },
-    buttonPressCart: {
-        borderRadius: 100,
-        height: '100%',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    imgBag: {
-        width: 40,
-        height: 40,
-    },
     modalContent: {
         backgroundColor: '#fff',
-        width: '87%',
         justifyContent: 'center',
         alignItems: 'center',
-        paddingBottom: '2%',
-        marginTop: -10,
+        paddingBottom: '2.5%',
+        marginTop: -12,
         borderBottomRightRadius: 10,
         borderBottomLeftRadius: 10,
+        width: '82%',
     },
     buttonPressAdd: {
         flexDirection: 'row',
         backgroundColor: 'white',
         width: '100%',
-        paddingLeft: '22%',
+        paddingLeft: '24%',
     },
-    textNameSkewer: {
-        color: '#000',
+    textNameSubTitle: {
+        color: COLORS.grayescuro,
         fontSize: 16,
-        fontWeight: 'bold',
-        marginLeft: '3%',
+        marginLeft: '5%',
+        fontFamily: 'Inter-Semibold',
+        paddingBottom: '5%',
+    },
+    viewTextTitle: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+    },
+    textTitle: {
+        fontSize: 18,
+        fontFamily: 'Inter-Semibold',
+        left: '16%'
     },
 })
 
