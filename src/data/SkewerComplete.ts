7ce5ce804ffffbff90c07b667aad29f5c203b442
SavioUFC <saviocarvalho@alu.ufc.br>
2023-12-28 22:21:26 -0300

    add file

---
+++ src/data/SkewerComplete.ts
@@ -0,0 +1,32 @@
+const SkewerComplete = [
+    {
+      id: 'SC1',
+      name: 'Espeto de Carne',
+      price: '18',
+      type: 'SkewerComplete',
+      index: 0,
+    },
+    {
+      id: 'SC2',
+      name: 'Espeto de Frango',
+      price: '18',
+      type: 'SkewerComplete',
+      index: 1,
+    },
+    {
+      id: 'SC3',
+      name: 'Espeto Suíno',
+      price: '18',
+      type: 'SkewerComplete',
+      index: 2,
+    },
+    {
+      id: 'SC4',
+      name: 'Espeto de Cupim',
+      price: '25',
+      type: 'SkewerComplete',
+      index: 3,
+    },
+  ];
+  export default SkewerComplete;
+  
