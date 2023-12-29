7f7524dd565410b5b88a7f16d63dd524c739d643
SavioUFC <saviocarvalho@alu.ufc.br>
2023-12-28 22:21:17 -0300

    add file

---
+++ src/data/Drinks.ts
@@ -0,0 +1,18 @@
+const Drinks = [
+    {
+      id: 'D1',
+      name: 'Refrigerante',
+      price: '5',
+      type: 'Drink',
+      index: 0,
+    },
+    {
+      id: 'D2',
+      name: 'Cerveja',
+      price: '5',
+      type: 'Drink',
+      index: 1,
+    },
+  ];
+  export default Drinks;
+  
