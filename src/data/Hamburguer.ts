11c58f424947433a910398f20fd58c9cdf12d0e6
SavioUFC <saviocarvalho@alu.ufc.br>
2023-12-29 08:15:27 -0300

    add file

---
+++ src/data/Hamburguer.ts
@@ -0,0 +1,11 @@
+const Hamburguer = [
+    {
+      id: 'HB1',
+      name: 'Hamburguer Completo',
+      price: '12',
+      type: 'Hamburguer',
+      index: 0,
+    },
+  ];
+  export default Hamburguer;
+  
