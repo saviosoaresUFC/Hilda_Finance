936ed0a540e6127f845b7fac545c3bb9583b652d
SavioUFC <saviocarvalho@alu.ufc.br>
2023-12-28 22:20:44 -0300

    add file

---
+++ src/data/Food.ts
@@ -0,0 +1,18 @@
+const Food = [
+    {
+      id: 'F1',
+      name: 'Arrumadinho Simples',
+      price: '12',
+      type: 'Food',
+      index: 0,
+    },
+    {
+      id: 'F2',
+      name: 'Arrumadinho Completo',
+      price: '15',
+      type: 'Food',
+      index: 1,
+    },
+  ];
+  export default Food;
+  
