f72e05f66231b0b1b725f7a0d7b91e6b6b7df25a
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-09 10:48:53 -0300

    edition file

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,64 +11,62 @@ import Drinks from '../data/Drinks';
 export const useStore = create(
     persist(
         (set, get) => ({
-            SkewerSimpleList: SkewerSimples,
-            SkewerCompleteList: SkewerComplete,
-            HamburguerList: Hamburguer,
-            FoodList: Food,
-            DrinksList: Drinks,
-            CartPrice: 0,
-            CartList: [],
-            addToCart: (cartItem: any) =>
+            SkewerSimpleList: SkewerSimples,    // Lista de Espetinhos Simples
+            SkewerCompleteList: SkewerComplete, // Lista de Espetinhos Completos
+            HamburguerList: Hamburguer,        // Lista de Hamburgueres
+            FoodList: Food,                   // Lista de Arrumadinho
+            DrinksList: Drinks,             // Lista de Bebidas
+            CartPrice: 0,               // Preço Total do Carrinho
+            CartList: [],           // Lista de Itens do Carrinho
+            addToCart: (cartItem: any) =>   // Adiciona um item ao carrinho
                 set(
-                    produce(state => {
-                        state.CartList.push(cartItem);
+                    produce(state => {  
+                        state.CartList.push(cartItem);  // Adiciona o item ao carrinho
                     }),
                 ),
-                calculateCartPrice: () =>
+                calculateCartPrice: () =>   // Calcula o preço total do carrinho
                 set(
                     produce(state => {
                         let totalprice = 0;
             
-                        for (let i = 0; i < state.CartList.length; i++) {
+                        for (let i = 0; i < state.CartList.length; i++) {   // Percorre a lista de itens do carrinho
                             let tempprice = 0;
-                            // Certifica que o preço é um número antes de adicionar
-                            if (!isNaN(parseFloat(state.CartList[i].price))) {
-                                tempprice = parseFloat(state.CartList[i].price);
-                            } else {
-                                console.error(`Preço para o item no índice ${i} não é um número válido`);
-                            }
-            
-                            state.CartList[i].ItemPrice = tempprice.toFixed(2).toString();
-                            totalprice = totalprice + tempprice;
+                            if (!isNaN(parseFloat(state.CartList[i].price))) { // Certifica que o preço é um número antes de adicionar
+                                tempprice = parseFloat(state.CartList[i].price);    // Adiciona o preço do item
+                            } 
+                            // else {
+                            //     console.error(`Preço para o item no índice ${i} não é um número válido`);
+                            // }
+                            state.CartList[i].ItemPrice = tempprice.toFixed(2).toString();  // Adiciona o preço do item
+                            totalprice = totalprice + tempprice;    // Adiciona o preço do item ao preço total
                         }
             
-                        state.CartPrice = totalprice.toFixed(2).toString();
+                        state.CartPrice = totalprice.toFixed(2).toString(); // Adiciona o preço total ao carrinho
                     }),
                 ),
-            cleanCartList: () =>
+            cleanCartList: () =>    // Limpa a lista de itens do carrinho e o preço total
                 set(
                     produce(state => {
-                        state.CartList = [];
-                        state.CartPrice = 0;
+                        state.CartList = [];   // Limpa a lista de itens do carrinho
+                        state.CartPrice = 0;    // Limpa o preço total
                     }),
                 ),
-            removeFromCart: (cartItem: any) =>
+            removeFromCart: (cartItem: any) =>  // Remove um item do carrinho e diminui o preço total
                 set(
                     produce(state => {
-                        for (let i = 0; i < state.CartList.length; i++) {
-                            if (state.CartList[i].id == cartItem.id && state.CartList[i].type == cartItem.type) {
-                                state.CartList.splice(i, 1);
-                                // diminui o valor do Price Total
-                                state.CartPrice = (parseFloat(state.CartPrice) - parseFloat(cartItem.ItemPrice)).toFixed(2).toString();
-                                break;
+                        for (let i = 0; i < state.CartList.length; i++) {   // Percorre a lista de itens do carrinho
+                            if (state.CartList[i].id == cartItem.id && state.CartList[i].type == cartItem.type) {   // Verifica se o item é o mesmo que o item a ser removido
+                                state.CartList.splice(i, 1);    // Remove o item do carrinho
+                                state.CartPrice = (parseFloat(state.CartPrice) - parseFloat(cartItem.ItemPrice)).toFixed(2).toString(); // diminui o valor do Price Total
+                                break;  // Para o loop
                             }
                         }
                     }),
                 ),
         }),
         {
-            name: 'Hilda_Finance',
-            storage: createJSONStorage(() => AsyncStorage),
+            name: 'Hilda_Finance',  // Nome do Storage
+            storage: createJSONStorage(() => AsyncStorage), // Armazena os dados no AsyncStorage do React Native
         },
     ),
 );
