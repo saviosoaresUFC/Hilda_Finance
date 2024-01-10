b09db3048c03282daebe3e85b88428e36c577fbd
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-10 09:13:15 -0300

    edition file

--- src/components/TableVendaDespesa.jsx
+++ src/components/TableVendaDespesa.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, ToastAndroid } from 'react-native';
 import { Table, Row, TableWrapper, Cell } from 'react-native-table-component';
 import { COLORS } from '../theme/theme';
-import { db, collection, getDocs } from '../../Firebase/firebaseConfig';
+import { useStore } from '../store/store';
 
 const TableVendaDespesa = () => {
+  const ListaVendas = useStore((state) => state.ListaVendas); // Lista de vendas
+  const ListaDespesas = useStore((state) => state.ListaDespesas); // Lista de despesas
   const [tableHead] = useState(['Mes', 'Venda', 'Despesa']);  // Cabeçalho da tabela
   const [tableData, setTableData] = useState([]); // Dados da tabela
 
   const getDataDB = async () => { // Função para buscar os dados no banco de dados
-    try {
-      const querySnapshot = await getDocs(collection(db, 'bdHildaFinance'));  // Busca os dados no banco de dados
       const newData = [ // Cria um array com os dados iniciais
         ['Janeiro', 'R$ ' + 0, 'R$ ' + 0],
         ['Fevereiro', 'R$ ' + 0, 'R$ ' + 0],
@@ -26,26 +26,32 @@ const TableVendaDespesa = () => {
         ['Dezembro', 'R$ ' + 0, 'R$ ' + 0],
       ];
 
-      querySnapshot.forEach((doc) => {  // Percorre os dados buscados no banco de dados
-        const monthIndex = newData.findIndex(row => row[0] === doc.data().month); // Busca o index do mês no array newData
-        if (monthIndex !== -1) {  // Se o mês existir no array newData
-          const isVenda = doc.data().hasOwnProperty('product'); // Decide se é Venda ou Despesa
-          const dataIndex = isVenda ? 1 : 2;  // Index do valor no array newData
-          const currentValue = parseFloat(newData[monthIndex][dataIndex].replace('R$ ', '')); // Valor atual no array newData
-          newData[monthIndex][dataIndex] = 'R$ ' + (currentValue + doc.data().value); // Atualiza o valor no array newData
-        }
-      });
-      setTableData(newData);  // Atualiza o estado com os novos dados
-    } catch (error) { // Caso ocorra algum erro
-      ToastAndroid.show('Erro ao buscar dados do banco de dados',
-        ToastAndroid.SHORT,
-        ToastAndroid.CENTER,
-      );
-    }
+      const calculateSum = (dataList, isVenda) => {
+        dataList.forEach((doc) => {
+          const monthIndex = newData.findIndex(row => row[0] === doc.month);
+          if (monthIndex !== -1) {
+            const dataIndex = isVenda ? 1 : 2;
+            const currentValue = parseFloat(newData[monthIndex][dataIndex].replace('R$ ', ''));
+            newData[monthIndex][dataIndex] = 'R$ ' + (currentValue + doc.value);
+          }
+        });
+      };
+      
+      calculateSum(ListaVendas, true);  // Calcular a soma de vendas      
+      calculateSum(ListaDespesas, false); // Calcular a soma de despesas
+      setTableData(newData);
+   
   };
-  useEffect(() => { // Busca os dados no banco de dados
-    getDataDB();  // Função para buscar os dados no banco de dados
-  }, [tableData]);  // Toda vez que o estado tableData for atualizado, a função getDataDB é chamada
+
+  useEffect(() => {
+    const fetchData = async () => { // Aguarde a atualização de ListaVendas e ListaDespesas antes de chamar getDataDB
+      await ListaVendas;
+      await ListaDespesas;
+      getDataDB();  // Função para buscar os dados
+    };
+
+    fetchData();
+  }, [ListaVendas, ListaDespesas]); 
 
 
   return (
@@ -65,14 +71,14 @@ const TableVendaDespesa = () => {
                 }}
                 style={{
                   ...styles.cell,
-                  ...(cellIndex === 1 ? (cellData === 'R$ 0' ? {...styles.vendaCell, opacity: 0.5} : styles.vendaCell) : {}),
-                  ...(cellIndex === 2 ? (cellData === 'R$ 0' ? {...styles.despesaCell, opacity: 0.5} : styles.despesaCell) : {}),
+                  ...(cellIndex === 1 ? (cellData === 'R$ 0' ? { ...styles.vendaCell, opacity: 0.5 } : styles.vendaCell) : {}),
+                  ...(cellIndex === 2 ? (cellData === 'R$ 0' ? { ...styles.despesaCell, opacity: 0.5 } : styles.despesaCell) : {}),
                 }}
               />
             ))}
           </TableWrapper>
         ))}
-      </Table> 
+      </Table>
     </>
   );
 };
@@ -124,7 +130,7 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: '8%',
     fontFamily: 'Poppins-Semibold',
-  }, 
+  },
 });
 
 export default TableVendaDespesa;
