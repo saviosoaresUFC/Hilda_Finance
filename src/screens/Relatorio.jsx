f7d53df2de086fd950b606ee930bb6ec6c098d35
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-13 13:55:20 -0300

    edition file

--- src/screens/Relatorio.jsx
+++ src/screens/Relatorio.jsx
@@ -3,22 +3,45 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import HeaderBar from '../components/HeaderBar'
 import TableVendaDespesa from '../components/TableVendaDespesa'
 import TableLucro from '../components/TableLucro'
+import GraficLM from '../components/GraficLM'
+import GraficVM from '../components/GraficVM'
+import { COLORS } from '../theme/theme'
 
 
-const Relatorio = () => {
+
+const Relatorio = ({ navigation }) => {
+
+  const avaliableKey = (amount) => {
+    if (amount === '1') {
+      navigation.push('Informations');
+    }
+  }
+
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
       <View style={styles.headerBar}>
-        <HeaderBar/>
+        <HeaderBar avaliableKey={avaliableKey} />
       </View>
       <View style={styles.title}>
         <Text style={styles.text}>Relatório Venda/Despesa</Text>
       </View>
       <TableVendaDespesa />
-      <View style={styles.title2}>
+      {/* <View style={styles.title2}>
         <Text style={styles.text}>Relatório Lucro</Text>
+      </View> */}
+      {/* <TableLucro /> */}
+      <View style={styles.title}>
+        <Text style={styles.text}>Vendas Mensais</Text>
       </View>
-      <TableLucro/>
+      <View style={styles.graficVM}>
+        <GraficVM />
+      </View>
+      <View style={[styles.title, {top: '1%'}]}>
+        <Text style={styles.text}>Lucros Mensais</Text>
+      </View>
+      <View style={styles.graficLM}>
+        <GraficLM />
+      </View>
     </ScrollView>
   );
 }
@@ -26,6 +49,7 @@ const Relatorio = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#C2C2C2',
+    marginBottom: '-30%',
   },
   headerBar: {
     height: '6.5%',
@@ -42,7 +66,29 @@ const styles = StyleSheet.create({
   title2: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: '12%',
+    // marginTop: '12%',
+  },
+  graficVM: {
+    marginBottom: '20%',
+    height: '14%',
+    padding: '4%',
+  },
+  graficLM: {
+    // marginBottom: '50%',
+    height: '100%',
+    padding: '4%',
+    top: '0.5%',
+  },
+  titleGrafic: {
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: '10%',
+    top: '2.5%',
+  },
+  titleText: {
+    color: COLORS.black,
+    fontFamily: 'Inter-Black',
+    fontSize: 18,
   },
 })
 
