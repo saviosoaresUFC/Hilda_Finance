f083cde6f614e2123ae4c5480c094aec8232023c
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-18 10:46:40 -0300

    edition file

--- src/data/SkewerSimple.ts
+++ src/data/SkewerSimple.ts
@@ -1,28 +1,28 @@
 const SkewerSimples = [
     {
       id: 'SS1',
-      name: 'Espeto de Carne',
+      name: 'Espeto de Carne(S)',
       price: '14',
       type: 'SkewerSimple',
       index: 0
     },
     {
       id: 'SS2',
-      name: 'Espeto de Frango',
+      name: 'Espeto de Frango(S)',
       price: '14',
       type: 'SkewerSimple',
       index: 1,
     },
     {
       id: 'SS3',
-      name: 'Espeto Suíno',
+      name: 'Espeto Suíno(S)',
       price: '14',
       type: 'SkewerSimple',
       index: 2,
     },
     {
       id: 'SS4',
-      name: 'Espeto de Cupim',
+      name: 'Espeto de Cupim(S)',
       price: '18',
       type: 'SkewerSimple',
       index: 3,
