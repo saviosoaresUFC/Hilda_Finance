0fafe32d2c9cb8c2665aa367f195e30d34354e9f
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-24 08:39:26 -0300

    edition file

--- src/screens/Edition.jsx
+++ src/screens/Edition.jsx
@@ -4,10 +4,11 @@ import { FontAwesome, Fontisto } from '@expo/vector-icons'
 import { COLORS, ICONS } from '../theme/theme'
 import { useStore } from '../store/store'
 import LottieView from 'lottie-react-native';
-import Octicons from '@expo/vector-icons/Octicons';
+import { Octicons, Ionicons } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur'
+import styles from './styles/EditionStyle'
 
-const Edition = () => {
+const Edition = ({navigation}) => {
   const [saleVisible, setSaleVisible] = useState(true)
   const [expenseVisible, setExpenseVisible] = useState(false)
   const [animation, setAnimation] = useState(false)
@@ -52,7 +53,13 @@ const Edition = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.viewTittle}>
+      <View style={styles.viewHeader}>
+        <TouchableOpacity style={styles.buttonBack}
+        onPress={() => {
+          navigation.pop()
+        }}>
+          <Ionicons name="chevron-back-outline" size={30} color="white" />
+        </TouchableOpacity>
         <Text style={styles.textTittle}>O que queres editar?</Text>
       </View>
       <View style={styles.choice}>
@@ -189,217 +196,5 @@ const Edition = () => {
 
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.orange,
-    top: '4%',
-  },
-  viewTittle: {
-    alignItems: 'center',
-    top: '1%',
-  },
-  textTittle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff'
-  },
-  choice: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    top: '8%',
-    height: '7%',
-  },
-  choiceSale: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  choiceExpense: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewSaleAndExpense: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    paddingLeft: '10%',
-    paddingRight: '10%',
-    borderBottomWidth: 4,
-  },
-  textSaleAndExpense: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  viewLottie: {
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    marginTop: '10%',
-    // backgroundColor: COLORS.gray,
-    width: '100%',
-    height: '70%',
-  },
-  viewTextEmpty: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    bottom: '40%',
-  },
-  textEmpty: {
-    fontSize: 18,
-    justifyContent: 'center',
-    top: '10%',
-    fontFamily: 'Inter-Semibold',
-    color: '#fff',
-  },
-
-  // SALE LIST
-  viewSaleContainer: {
-    alignItems: 'center',
-    marginTop: '16%',
-    // backgroundColor: COLORS.gray,
-    width: '100%',
-    height: '82%',
-  },
-  viewSaleCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
-    height: '6%',
-    borderRadius: 10,
-    backgroundColor: '#fff',
-    marginTop: '2%',
-  },
-  viewSaleCardExpense: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
-    height: '6%',
-    borderRadius: 10,
-    // backgroundColor: '#fff',
-    marginTop: '2%',
-  },
-  viewSaleButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '20%',
-    height: '100%',
-
-  },
-  buttonRemove: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'black',
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-  },
-  viewSaleText: {
-    alignItems: 'flex-start',
-    // justifyContent: 'center',
-    flexDirection: 'row',
-    width: '80%',
-    height: '100%',
-    left: '5%',
-  },
-  viewExpenseText: {
-    alignItems: 'flex-start',
-    // justifyContent: 'center',
-    flexDirection: 'row',
-    width: '80%',
-    height: '100%',
-    left: '5%',
-  },
-  textSaleName: {
-    fontSize: 17,
-    fontFamily: 'Inter-Semibold',
-    color: COLORS.orange,
-  },
-  viewSaleTextValue: {
-    fontSize: 17,
-    fontFamily: 'Inter-Semibold',
-    color: '#fff',
-  },
-  textSaleQnt: {
-    fontSize: 12,
-    fontFamily: 'Inter-Semibold',
-    color: COLORS.orange,
-    left: '20%',
-  },
-  viewSaleTextName: {
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    width: '74%',
-    height: '100%',
-  },
-  viewSaleTextQnt: {
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    width: '18%',
-    height: '100%',
-  },
-  lottie: {
-    position: 'absolute',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-  },
-
-
-  // EXPENSE LIST
-  containerExpense: {
-    // backgroundColor: COLORS.gray,
-    width: '100%',
-    height: '78%',
-    marginTop: '14%',
-    marginBottom: '20%',
-  },
-  itensExpense: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: '4%',
-    // backgroundColor: '#fff',
-    marginTop: '2%',
-  },
-  buttonRemoveExpense: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '10%',
-  },
-  viewExpenseValue: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '30%',
-    borderBottomWidth: 4,
-    borderColor: '#fff',
-    borderRadius: 10,
-    padding: '1%',
-  },
-  textExpenseValue: {
-    fontSize: 18,
-    fontFamily: 'Inter-Semibold',
-    color: '#fff',
-  },
-  viewExpenseMonth: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '30%',
-    borderBottomWidth: 4,
-    borderColor: '#fff',
-    borderRadius: 10,
-    padding: '1%',
-  },
-  textExpenseMonth: {
-    fontSize: 18,
-    fontFamily: 'Inter-Semibold',
-    color: '#fff',
-  },
-})
 
 export default Edition
